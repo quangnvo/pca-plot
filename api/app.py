87440950b73571beb63516601e7e8674741a8504
quangnvo <voquang.usth@gmail.com>
2024-01-26 11:37:51 +0100

    add the root app.py and add the blueprint for the generateSampleData

---
+++ api/app.py
@@ -0,0 +1,19 @@
+import generateSampleData
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+PORT = 8080
+
+
+@app.route('/')
+def home():
+    return f"Flask server is running on port {PORT}!"
+
+
+app.register_blueprint(generateSampleData.bp)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=PORT, debug=True)
--- api/generateSampleData.py
+++ api/generateSampleData.py
@@ -2,20 +2,24 @@ import random as rd
 
 import numpy as np
 import pandas as pd
-from flask import Flask
+from flask import Blueprint, Flask
 from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+bp = Blueprint('generateSampleData', __name__)
 
 
-@app.route('/api/generate_data', methods=['GET'])
+@bp.route('/api/generate_data', methods=['GET'])
 def generate_data():
     genes = ['gene' + str(i) for i in range(1, 101)]
     wt = ['wt' + str(i) for i in range(1, 6)]
     ko = ['ko' + str(i) for i in range(1, 6)]
+
+    # Use * to unpack the lists
+    # If not using *, it will be [['wt1', 'wt2', 'wt3','wt4',,'wt5'], ['ko1', 'ko2', 'ko3', 'ko4', 'ko5']]
+    # But we want ['wt1', 'wt2', 'wt3', 'wt4', 'wt5', 'ko1', 'ko2', 'ko3', 'ko4', 'ko5']
     data = pd.DataFrame(columns=[*wt, *ko], index=genes)
 
+    print("\n Data before random generation \n")
     print(genes)
     print("**************")
     print(wt)
@@ -30,7 +34,8 @@ def generate_data():
             lam=rd.randrange(10, 1000), size=5)
         data.loc[gene, 'ko1':'ko5'] = np.random.poisson(
             lam=rd.randrange(10, 1000), size=5)
+
+    print("\n Data after random generation \n")
+    print(data)
+
     return data.to_json(orient='split')
-
-
-generate_data()
