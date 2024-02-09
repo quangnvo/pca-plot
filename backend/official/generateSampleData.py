c51795c9d6524ffc150e19d398ba09a031934f3b
quangnvo <voquang.usth@gmail.com>
2024-02-09 10:54:13 +0100

    finish the generateSampleData

--- backend/official/generateSampleData.py
+++ backend/official/generateSampleData.py
@@ -15,12 +15,16 @@ def generate_data():
     # Generate 300 random samples
     for _ in range(300):
         sample = {
-            # The locus tag is a random 10-character string of uppercase letters and digits
-            'locus tag': ''.join(random.choices(string.ascii_uppercase + string.digits, k=10)),
-            'logFC': f"{random.uniform(-2, 2):.2f}",
-            'logCPM': f"{random.uniform(3, 7):.2f}",
-            'PValue': f"{random.uniform(0, 1):.2f}",
-            'FDR': f"{random.uniform(0, 1):.2f}"
+            # Generate a random 10-character string of uppercase letters and digits (to make it as a name)
+            'Gene': ''.join(random.choices(string.ascii_uppercase + string.digits, k=10)),
+
+            # Generate random values for the 4 conditions
+            # f"" is a string formatting method that is used to insert the value into a string
+            'condition 1': f"{random.uniform(-2, 2):.2f}",
+            'condition 2': f"{random.uniform(3, 7):.2f}",
+            'condition 3': f"{random.uniform(0, 1):.2f}",
+            'condition 4': f"{random.uniform(0, 1):.2f}",
+            'condition 5': f"{random.uniform(4, 10):.2f}"
         }
         data.append(sample)
     return jsonify(data)
