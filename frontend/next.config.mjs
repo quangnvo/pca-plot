86ea4ed30042804a430683dc6e6151ea5b5a483f
quangnvo <voquang.usth@gmail.com>
2024-03-28 15:55:45 +0100

    modify gitignore and next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Put "export" in the "output" to make the folder "out" after running "next build"
+    // Put "export" in the "output" to generate the folder "out" after running "npm run build"
     output: 'export',
 };
 
