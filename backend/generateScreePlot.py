1d86e670b83f858fc14b8deb8594ac8af30e6d1d
quangnvo <voquang.usth@gmail.com>
2024-04-03 14:46:12 +0200

    add numpy

--- backend/generateScreePlot.py
+++ backend/generateScreePlot.py
@@ -10,6 +10,7 @@
 # Check the file "generatePCA.py" for the detail explanation, as almost the code here is similar to the code in "generatePCA.py"
 #########################
 
+import numpy as np
 import pandas as pd
 from flask import Blueprint, jsonify, request
 from sklearn.decomposition import PCA
