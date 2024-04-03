bb74121375e98ec2f9ad2301fda244bbc654f337
quangnvo <voquang.usth@gmail.com>
2024-04-03 16:43:42 +0200

    modify next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    // Put "export" in the "output" to generate the folder "out" after running "npm run build"
-    output: 'export',
-};
+const nextConfig = {};
 
 export default nextConfig;
